fd7abdfff9d7da5a8837ab1e213a3ddf7dea626c
Gautam <gautamkumar929982@gmail.com>
2024-06-20 13:07:09 +0530

    first commit of my portfolio website

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const body_parser = require('body_parser');
+const app = express();
+const port = 3000;
+
